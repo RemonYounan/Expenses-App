bf1c89778401e9029415e2c7655c5feefb65d3d6
RemonYounan <reemooyounan@gmail.com>
2022-07-01 16:00:46 +0200

    edited

--- lib/screens/notifications_screen.dart
+++ lib/screens/notifications_screen.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
-class NotificationsScreen extends StatelessWidget {
+class NotificationsScreen extends StatefulWidget {
   const NotificationsScreen({Key? key}) : super(key: key);
 
   static const routeName = '/notifications-screen';
+
+  @override
+  State<NotificationsScreen> createState() => _NotificationsScreenState();
+}
+
+class _NotificationsScreenState extends State<NotificationsScreen> {
+  var _readed = false;
   @override
   Widget build(BuildContext context) {
     // final notification = Provider.of<Notifications>(context); // can't get provider
@@ -36,26 +43,35 @@ class NotificationsScreen extends StatelessWidget {
                   Text(
                     'Getting started',
                     textAlign: TextAlign.left,
-                    style: Theme.of(context).textTheme.titleLarge,
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(color: _readed ? Colors.grey : Colors.white),
                   ),
-                  const Text(
+                  Text(
                     'This application is about calculating your expenses and income and shows how much you saved in the month and year.',
                     style: TextStyle(
-                      color: Colors.white,
+                      color: _readed ? Colors.grey : Colors.white,
                     ),
                     softWrap: true,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          'Mark as read',
-                        ),
-                      ),
-                    ],
-                  ),
+                  !_readed
+                      ? Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  _readed = !_readed;
+                                });
+                              },
+                              child: const Text(
+                                'Mark as read',
+                              ),
+                            ),
+                          ],
+                        )
+                      : Container(),
                 ],
               ),
             ),
